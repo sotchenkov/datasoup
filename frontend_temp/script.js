b7050057e87da1fc3d02d941807d7e9a66274869
Alexey <sotchenkoff@gmail.com>
2023-02-25 00:41:03 +0300

    Add js, new cards and animations

---
+++ frontend_temp/script.js
@@ -0,0 +1,30 @@
+let card = document.querySelector(".card");
+let card_text = document.querySelector(".card-text");
+let ozon_inputs = document.querySelector(".ozon-inputs");
+let ozon_contents = document.querySelector(".ozon-card-background");
+
+let is_clicked = false;
+
+
+document.querySelector('.yandex').style.display = 'none';
+document.querySelector('.google').style.display = 'none';
+
+ozon_contents.addEventListener("click", function () {
+  if (!is_clicked) {
+    card_text.style.display = "none";
+    ozon_inputs.style.display = 'inline-block';
+    document.querySelector('.yandex').style.display = 'inline-block';
+    document.querySelector('.google').style.display = 'inline-block';
+    document.querySelector('.instruction').style.display = 'none';
+    document.querySelector('.ozon-token').focus();
+    is_clicked = true;
+  }
+  else {
+    card_text.style.display = "inline-block";
+    ozon_inputs.style.display = 'none';
+    document.querySelector('.yandex').style.display = 'none';
+    document.querySelector('.google').style.display = 'none';
+    document.querySelector('.instruction').style.display = '';
+    is_clicked = false;
+  }
+});
