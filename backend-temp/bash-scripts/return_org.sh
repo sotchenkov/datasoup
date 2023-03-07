1a7e5a0bbcfb2b34c6ebac88f99ade8824ac0bd1
Alexey <sotchenkoff@gmail.com>
2023-03-07 23:49:55 +0300

    Added a request to the function of getting the user's organization + displaying organizations

--- backend-temp/bash-scripts/return_org.sh
+++ backend-temp/bash-scripts/return_org.sh
@@ -3,8 +3,7 @@ set -e
 
 access_token=$(cat | jq '.' | jq '.queryStringParameters .access_token' | jq -r)
 
-get_orgs=$(yc organization-manager organization list --token=$access_token --format json | jq .'[]')
+get_orgs=$(yc organization-manager organization list --token=$access_token --format json | jq -c)
 
-RESPONSE=$(cat | jq -sc '.[0] // {}' | jq --arg orgs "$get_orgs" -c '{statusCode:200, body:{orgs:$orgs}}')
-echo $RESPONSE >&2
-echo $RESPONSE | jq -c '.body |= tostring' # make sure 'body' is a string, not a json node
+RESPONSE=$(cat | jq -s --argjson orgs "$get_orgs" '{statusCode:200, body:{orgs:$orgs}}')
+echo $RESPONSE | jq -c '.body |= tostring' # make sure is a string, not a json node
